74be3e21b20a6acb0e82df89d8736ec31310f413
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2021-12-16 17:21:09 -0600

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// variables for page elements 
+
+// variables for buttons  
+// start quiz btn
+var startquizbtn = document.querySelector(".startQuizBtn");
+// answer buttons
+// submit button
+// back button
+
+// create object variables for the questions
+var questions = [
+    {
+        question: "What is 2 + 2 ?",
+        answers: { a: "5", b: "3", c: "4" },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is 3 + 1 ?",
+        answers: { a: "9", b: "4", c: "7" },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is 10 - 2 ?",
+        answers: { a: "6", b: "8", c: "2" },
+        correctAnswer: "b"
+    },
+]
+
+// function to start quiz
+var startQuiz = function ();
+for (var i = 0; i < questions.length; i++) {
+    options += <
+}
+
