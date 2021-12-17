3ce7fcecf6be225677731c26e33f91a5cfb744e6
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2021-12-16 21:24:54 -0600

    added game and high score html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,110 @@
 // variables for page elements 
+var question = document.querySelector("#question")
+var choice = document.querySelector(".choice-text")
+var timer = document.querySelector("#timer")
 
-// variables for buttons  
-// start quiz btn
-var startquizbtn = document.querySelector(".startQuizBtn");
-// answer buttons
-// submit button
-// back button
+var currentQuestion = []
+var score = 0
+var questionCounter = 0
+var availableQuestions = []
 
-// create object variables for the questions
+var scorePoints = 100
+var maxQuestions = 4
+
+// create object array for the questions
 var questions = [
     {
         question: "What is 2 + 2 ?",
-        answers: { a: "5", b: "3", c: "4" },
-        correctAnswer: "c"
+        choice: {
+            choice1: "5",
+            choice2: "3",
+            choice3: "4",
+            choice4: "9"
+        },
+        correctAnswer: 2
     },
     {
-        question: "What is 3 + 1 ?",
-        answers: { a: "9", b: "4", c: "7" },
-        correctAnswer: "b"
+        question: "What is 9 + 2 ?",
+        choice: {
+            choice1: "11",
+            choice2: "3",
+            choice3: "4",
+            choice4: "9"
+        },
+        correctAnswer: 1
     },
     {
-        question: "What is 10 - 2 ?",
-        answers: { a: "6", b: "8", c: "2" },
-        correctAnswer: "b"
+        question: "What is 10 + 2 ?",
+        choice: {
+            choice1: "5",
+            choice2: "3",
+            choice3: "12",
+            choice4: "9"
+        },
+        correctAnswer: 3
     },
-]
+    {
+        question: "What is 11 + 2 ?",
+        choice: {
+            choice1: "5",
+            choice2: "3",
+            choice3: "4",
+            choice4: "13"
+        },
+        correctAnswer: 4
+    }
+];
 
-// function to start quiz
-var startQuiz = function ();
-for (var i = 0; i < questions.length; i++) {
-    options += <
+// Timer/countdown function 
+function countdown() {
+    var timeLeft = 7; //change this to 100 later 
+
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timer.textContent = "Time: " + timeLeft + " seconds remaining "
+            timeLeft--
+        } else if
+            (timeLeft === 1) {
+            timer.textContent = "Time: " + timeLeft + " second remaining "
+            timeLeft--
+        }
+        else {
+            timer.textContent = " "
+            clearInterval(timeInterval)
+            timer.textContent = "You ran out of time!"
+        }
+    }, 1000);
 }
 
+
+var startQuiz = function () {
+    questionCounter = 0
+    availableQuestions = [...questions]
+    getQuestion()
+}
+
+var getQuestion = function () {
+    // logs score to local storage
+    if (availableQuestions.length === 0 || questionCounter > maxQuestions) {
+        localStorage.setItem("mostRecentScore", score)
+        return window.location.assign("/end.html")
+    }
+
+    questionCounter++
+
+    var randomQuestion = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[randomQuestion]
+    question.innerText = currentQuestion.question
+
+
+}
+
+//for (i = 0; i < questions.length; i++) {
+//}
+
+// call the start quiz function to start the quiz
+startQuiz()
+
+// call the countdown function to start the timer
+countdown()
